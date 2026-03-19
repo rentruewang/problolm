9ada1b0e45105b38ea74cfa35c05a2886a7bd69b
RenChu Wang <patrick1031wang@gmail.com>
2026-03-18 19:53:10 -0700

    tests

--- tests/test_commits.py
+++ tests/test_commits.py
@@ -5,15 +5,25 @@ import pytest
 from problolm import Commit
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def commit():
     return Commit("3cd26e765f27ab2a40ea322c1beb8feac3389d70")
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def short_commit():
     return Commit("3cd26e7")
 
 
+@pytest.fixture(scope="module")
+def parent(commit):
+    return commit.parent
+
+
 def test_commits_eq(commit, short_commit):
     assert commit == short_commit
+
+
+def test_commit_ne(commit, parent):
+    assert commit != parent
+    assert commit.parent == parent
--- tests/test_ok.py
+++
@@ -1,5 +0,0 @@
-# Copyright (c) ProBloLM Authors - All Rights Reserved
-
-
-def test_ok():
-    "The placeholder test to prevent pytest from erroring before real test cases."
